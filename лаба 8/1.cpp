e6da3c2c6d480db7a222aa99e6431586f0fbe3bd
ifeler <115542692+ifeler@users.noreply.github.com>
2022-10-31 20:43:14 +0700

    Add files via upload

---
+++ лаба 8/1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+const int n = 10;
+main()
+{ system("CLS"); 
+float a[n+1],c ,b, x,h,s ;
+int i;
+c=-2;b=2;
+x=c;
+h=(b-c)/n;
+for (i=0;i<=n;i++){
+	a[i]=(1+2*x+x*x)/(5+2*x*x);
+	if ((i==0) or (i==n)){
+		s+=a[i]/2;
+	}
+	else s+=a[i];
+	x+=h;
+}
+s*=h;
+printf("%f",s);
+printf("\n");	
+system("PAUSE");
+return 0;
+	
+}
